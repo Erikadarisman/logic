7e6bbbb6c246d1b5e1937b5da8e634533b73ecb9
kdrsm <eryx466@gmail.com>
2019-07-27 15:50:56 +0700

    update

--- nomer6.js
+++ nomer6.js
@@ -1,6 +1,28 @@
 arkaFood = (total,voc,jarak,pajak) => {
     let bayar
-    
+    if (voc != false) {
+        let q
+        if (voc == 'ARKAFOOD5') {
+            if (bayar >= 50000) {
+                q = total*50/100
+                if (q>50000) {
+                    q = 50000
+                }
+            }
+        }
+        if(voc == 'DITRAKTIRDEMY'){
+            if (bayar >= 30000) {
+                q = total*60/100
+                if (q > 30000) {
+                    q = 30000
+                }
+            }
+        }
+    }
+    if (condition) {
+        
+    }
+
 
 }
 
